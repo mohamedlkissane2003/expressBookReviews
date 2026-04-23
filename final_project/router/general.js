f9b26330aa318be3c676a9eaeb7b749889b19fd1
Mohamed Lkissane <medoozwagra2003@gmail.com>
2026-04-23 21:40:44 +0100

    Complete tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,65 +6,76 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  const { username, password } = req.body;
-
-  if (!username || !password) {
-    return res.status(400).json({ message: "Username and password required" });
-  }
-
-
-  if (!isValid(username)) {
-    return res.status(404).json({ message: "User already exists" });
-  }
-
-  users.push({ username, password });
-
-  return res.status(200).json({ message: "User registered successfully" });
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
+public_users.get('/', async function (req, res) {
+  const getBooks = new Promise((resolve, reject) => {
+    resolve(books);
+  });
+
+  let data = await getBooks;
+  return res.status(200).json(data);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
-  return res.status(200).json(books[isbn]);
- });
+
+  const getBook = new Promise((resolve, reject) => {
+    resolve(books[isbn]);
+  });
+
+  let data = await getBook;
+  return res.status(200).json(data);
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
-  let result = [];
 
-  for (let key in books) {
-    if (books[key].author === author) {
-      result.push(books[key]);
+  const getBooksByAuthor = new Promise((resolve, reject) => {
+    let result = [];
+
+    for (let key in books) {
+      if (books[key].author === author) {
+        result.push(books[key]);
+      }
     }
-  }
 
-  return res.status(200).json(result);
+    resolve(result);
+  });
+
+  let data = await getBooksByAuthor;
+  return res.status(200).json(data);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title;
-  let result = [];
 
-  for (let key in books) {
-    if (books[key].title === title) {
-      result.push(books[key]);
+  const getBooksByTitle = new Promise((resolve, reject) => {
+    let result = [];
+
+    for (let key in books) {
+      if (books[key].title === title) {
+        result.push(books[key]);
+      }
     }
-  }
 
-  return res.status(200).json(result);
+    resolve(result);
+  });
+
+  let data = await getBooksByTitle;
+  return res.status(200).json(data);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  return res.status(200).json(books[isbn].reviews);
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
