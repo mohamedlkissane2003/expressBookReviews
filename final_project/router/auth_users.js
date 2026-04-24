cb0b8ad66c5a6a020cce08a6bca376042e830e05
Mohamed Lkissane <medoozwagra2003@gmail.com>
2026-04-24 21:45:55 +0100

    Complete tasks

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -64,7 +64,7 @@ regd_users.delete("/auth/review/:isbn", (req, res) => {
 
   delete books[isbn].reviews[username];
 
-  return res.status(200).json({ message: "Review deleted successfully" });
+  return res.status(200).json({ message: `Review for ISBN ${isbn} deleted` });
 });
 
 module.exports.authenticated = regd_users;
